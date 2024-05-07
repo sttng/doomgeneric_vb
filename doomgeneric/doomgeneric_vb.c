6225eae76ff239cb4d032bd1123356fe27e4b548
PizzaRollsRoyce <me@pizzarollsroyce.com>
2024-05-07 12:38:25 -0500

    OD support

--- doomgeneric/doomgeneric_vb.c
+++ doomgeneric/doomgeneric_vb.c
@@ -5,40 +5,25 @@
 #include "doomkeys.h"
 #include "i_video.h"
 
-volatile uint32_t vb_ms;
+#ifdef VB_OVERDRIVE
+#include <sys/time.h>
+#include <stdio.h>
+#include <fcntl.h>
 
-//#define RENDER3D_GRAYSCALE 0
+int vb_fd;
+struct timeval launch_tv;
 
-static const int DITHER_MATRIX[8][8] = {
-    // Premultiply by 3 since we will divide by 3 later.
-    {0 * 3, 48 * 3, 12 * 3, 60 * 3, 3 * 3, 51 * 3, 15 * 3, 63 * 3},
-    {32 * 3, 16 * 3, 44 * 3, 28 * 3, 35 * 3, 19 * 3, 47 * 3, 31 * 3},
-    {8 * 3, 56 * 3, 4 * 3, 52 * 3, 11 * 3, 59 * 3, 7 * 3, 55 * 3},
-    {40 * 3, 24 * 3, 36 * 3, 20 * 3, 43 * 3, 27 * 3, 39 * 3, 23 * 3},
-    {2 * 3, 50 * 3, 14 * 3, 62 * 3, 1 * 3, 49 * 3, 13 * 3, 61 * 3},
-    {34 * 3, 18 * 3, 46 * 3, 30 * 3, 33 * 3, 17 * 3, 45 * 3, 29 * 3},
-    {10 * 3, 58 * 3, 6 * 3, 54 * 3, 9 * 3, 57 * 3, 5 * 3, 53 * 3},
-    {42 * 3, 26 * 3, 38 * 3, 22 * 3, 41 * 3, 25 * 3, 37 * 3, 21 * 3}};
-
-int Dither24(int x, int y, struct color c) {
-#if 0
-  int r = (uint16_t)(c.r >> (8 - s_Fb.red.length));
-  int g = (uint16_t)(c.g >> (8 - s_Fb.green.length));
-  int b = (uint16_t)(c.b >> (8 - s_Fb.blue.length));
-#else
-  int r = (uint16_t)(c.r);
-  int g = (uint16_t)(c.g);
-  int b = (uint16_t)(c.b);
-#endif
-  const int sum = r + g + b + DITHER_MATRIX[x & 7][y & 7];
-
-  // Little trick to divide by 3 by multiplying by its fixed-point reciprocal.
-  int final_val = (sum * 0x5555) >> (6 + 16);
-  if (final_val > 3) {
-    final_val = 3;
+void DG_Init() {
+  vb_fd = open("/dev/virtualboy", O_SYNC | O_RDWR);
+  if (vb_fd < 0) {
+    fputs("Unable to open /dev/virtualboy\n", stderr);
+    abort();
   }
-  return final_val;
+  gettimeofday(&launch_tv, NULL);
 }
+
+#else // !VB_OVERDRIVE
+volatile uint32_t vb_ms;
 
 // VB Hardware registers.
 volatile uint16_t *tcr = (volatile uint16_t *)(0x02000020);
@@ -69,6 +54,8 @@ void DG_Init() {
   *scr = 0b10000100;
 }
 
+#endif // !VB_OVERDRIVE
+
 enum vb_btn_t {
   VB_BTN_PWR,
   VB_BTN_SGN,
@@ -89,53 +76,36 @@ enum vb_btn_t {
 };
 
 uint16_t buttons_down;
-extern uint8_t *I_VideoBuffer;
 
 void DG_DrawFrame() {
+#ifdef VB_OVERDRIVE
+  read(vb_fd, &buttons_down, sizeof(buttons_down));
+#else
   if (!(*scr & 0b10)) {
     // Copy current button state.
     buttons_down = *sdlr | (*sdhr << 8);
     // Initiate reading next button state.
     *scr = 0b10000100;
   }
-
-  // TODO: Use parallax
-  // TODO: Use VIP state to select framebuffer
-  volatile uint16_t *fbl = (volatile uint16_t *)(0x00000000);
-  volatile uint16_t *fbr = (volatile uint16_t *)(0x00010000);
-
-#if 0
-  // TODO: Update I_FinishUpdate to directly output to
-  // the VB FrameBuffer and skip double (triple?) buffering
-  for (int x = 32; x < 352; x++) {
-    int y_off = 0;
-    for (int octcol = 1; octcol < 26; ++octcol) {
-      uint16_t bits = 0;
-      for (int cy = 0; cy < 8; cy++, y_off += 320) {
-        const int y = (octcol << 3) + cy;
-        const uint8_t pal_idx = I_VideoBuffer[y_off + x - 32];
-        const struct color px32 = colors[pal_idx];
-        bits >>= 2;
-        bits |= (Dither24(x, y, px32) << 14);
-        //bits |= (pal_idx & 0b11) << 14; // Show something discernable
-        //bits |= (px32.b & 0b11) << 14;
-      }
-      const int fb_index = (x << 5) + octcol;
-      fbl[fb_index] = fbr[fb_index] = bits;
-    }
-  }
-#else
-#if 0
-  // Clear it.
-  for (int x = 32; x < 352; x++) {
-    for (int octcol = 1; octcol < 26; ++octcol) {
-      const int fb_index = (x << 5) + octcol;
-      fbl[fb_index] = fbr[fb_index] = 0;
-    }
-  }
-#endif
 #endif
 }
+
+#ifdef VB_OVERDRIVE
+void DG_SleepMs(uint32_t ms) {
+  usleep(ms*1000);
+}
+
+static int64_t ms_of(const struct timeval* tv) {
+  return (((int64_t)tv->tv_sec) * 1000) + (tv->tv_usec / 1000);
+}
+
+uint32_t DG_GetTicksMs() {
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  return ms_of(&now) - ms_of(&launch_tv);
+}
+
+#else  // !VB_OD
 
 void DG_SleepMs(uint32_t ms) {
   uint32_t target_ms = vb_ms + ms;
@@ -145,6 +115,7 @@ void DG_SleepMs(uint32_t ms) {
 }
 
 uint32_t DG_GetTicksMs() { return vb_ms; }
+#endif // !VB_OD
 
 unsigned char DoomKeyFor(enum vb_btn_t btn) {
   const char NO_OP = 'g';
@@ -152,17 +123,17 @@ unsigned char DoomKeyFor(enum vb_btn_t btn) {
   default:
   case VB_BTN_PWR:
   case VB_BTN_SGN:
-      return NO_OP;
+    return NO_OP;
   case VB_BTN_A:
-      return KEY_USE;
+    return KEY_USE;
   case VB_BTN_B:
-      return KEY_FIRE;
+    return KEY_FIRE;
   case VB_BTN_RT:
-      return KEY_FIRE;
+    return KEY_FIRE;
   case VB_BTN_LT:
-      return KEY_USE;
+    return KEY_USE;
   case VB_BTN_RU:
-      return NO_OP;
+    return NO_OP;
     break;
   case VB_BTN_RR:
     return KEY_STRAFE_R;
@@ -216,22 +187,36 @@ int main() {
   };
   doomgeneric_Create(4, argv);
 
+#ifdef VB_OVERDRIVE
+  uint16_t both[2 * (384 * 224 / 4)];
+  if (sizeof(both) != 43008) {
+    fprintf("Unexpected size of frame buffer. Wanted %d, got %d", 43008, sizeof(both));
+    abort();
+  }
+  for (;;) {
+    doomgeneric_Tick(both, &both[sizeof(both)/4]);
+    write(vb_fd, both, sizeof(both));
+  }
+#else // !VB_OVERDRIVE
 #ifdef VB_DOUBLE_BUFFER
-  uint16_t left[384*256/4];
-  uint16_t right[384*256/4];
+  uint16_t left[384 * 256 / 4];
+  uint16_t right[384 * 256 / 4];
   for (;;) {
     doomgeneric_Tick(left, right);
     memcpy(0, left, sizeof(left));
     memcpy(0x10000, right, sizeof(right));
   }
-#else
+#else // !VB_DOUBLE_BUFFER
   for (;;) {
-    doomgeneric_Tick((uint16_t*)0, (uint16_t*)0x10000);
+    doomgeneric_Tick((uint16_t *)0, (uint16_t *)0x10000);
   }
-#endif
+#endif // !VB_DOUBLE_BUFFER
+#endif // !VB_OVERDRIVE
 
   return 0;
 }
+
+#ifndef VB_OVERDRIVE
 
 int mkdir(const char *pathname, mode_t mode) {
   // Stub to make linker happy
@@ -245,9 +230,8 @@ void puts(char *s) {}
 #ifdef __GNUC__
 // Hopefully never called...
 int __muldi3(int a, int b) {
-  for(;;)
+  for (;;)
     ;
-  
 }
 #endif
 
@@ -309,3 +293,4 @@ void __attribute__((interrupt)) VbPlus_DuplexedException() {
   for (;;)
     ;
 }
+#endif // !VB_OVERDRIVE
